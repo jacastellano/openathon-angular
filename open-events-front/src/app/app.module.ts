22ef290d3e4c6e9bbe16c77e8384337bf550fa5c
José A. Castellano <jacastellanoa@gmail.com>
2019-06-10 22:25:31 +0200

    Lab 2

--- open-events-front/src/app/app.module.ts
+++ open-events-front/src/app/app.module.ts
@@ -5,17 +5,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    EventListComponent,
+    EventDetailsComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +28,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MatToolbarModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
