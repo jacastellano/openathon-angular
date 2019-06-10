1821b74631ad779ad5260151b7f52df97ce599c2
José A. Castellano <jacastellanoa@gmail.com>
2019-06-11 00:04:19 +0200

    Lab 3

--- open-events-front/src/app/app.module.ts
+++ open-events-front/src/app/app.module.ts
@@ -1,34 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import 'hammerjs';
+// Modules
+import { SharedModule } from "./shared/shared.module";
+import { EventsModule } from "./events/events.module";
+import { LoginModule } from "./login/login.module";
+import { ProfileModule } from "./profile/profile.module";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { EventListComponent } from './event-list/event-list.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
+// Components
+import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     ToolbarComponent,
-    EventListComponent,
-    EventDetailsComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatListModule,
-    MatCardModule,
+    AppRoutingModule,
+    SharedModule,
+    EventsModule,
+    LoginModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
