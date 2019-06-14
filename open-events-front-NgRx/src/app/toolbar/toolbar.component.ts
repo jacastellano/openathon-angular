ad553ec2e132010f3aa69da255f1d759f4c5718a
jacastellano <jacastellanoa@gmail.com>
2019-06-14 10:43:08 +0200

    Lab 6 - Subscribing to changes

--- open-events-front-NgRx/src/app/toolbar/toolbar.component.ts
+++ open-events-front-NgRx/src/app/toolbar/toolbar.component.ts
@@ -1,37 +1,50 @@
-import { Component, DoCheck } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from "../models/user";
 import { UserService } from "../core/user.service";
 import { Router } from "@angular/router";
+import { SubscriptionLike } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import * as login from '../store/login/login.actions';
 
 @Component({
   selector: 'oevents-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements DoCheck {
+export class ToolbarComponent implements OnInit, OnDestroy {
+
   user: User;
   isAuthenticated: boolean;
+  subscriptionLogin: SubscriptionLike;
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private store: Store<any>
   ) { }
-
-  ngDoCheck() {
-    this.checkUser();
-  }
-
-  checkUser() {
-    this.isAuthenticated = this.userService.checkUser();
-    if (this.isAuthenticated) {
-      this.user = JSON.parse(localStorage.getItem("user"));
-    }
-  }
 
   logout() {
     this.userService.logout();
-    this.isAuthenticated = false;
+    this.store.dispatch(new login.Logged(false));
     this.router.navigate(["/home"]);
+  }
+
+  ngOnInit(): void {
+
+    this.subscriptionLogin = this.store.pipe(select('login')).subscribe(
+      (state) => {
+        if (state) {
+          this.isAuthenticated = state.logged;
+          if (this.isAuthenticated) {
+            this.user = JSON.parse(localStorage.getItem("user"));
+          }
+        }
+      });
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptionLogin.unsubscribe()
   }
 
 }
