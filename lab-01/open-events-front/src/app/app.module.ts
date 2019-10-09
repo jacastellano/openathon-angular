ec8f8b5990cf83c8f3a968771b54c1f7a4b2d54e
José A. Castellano <jacastellanoa@gmail.com>
2019-10-09 22:16:42 +0200

    Landing page & Angular Material Setup

--- lab-01/open-events-front/src/app/app.module.ts
+++ lab-01/open-events-front/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
