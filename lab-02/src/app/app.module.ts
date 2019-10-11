f8e878062000c33f7363008cf4e7a199a741cb1c
José A. Castellano <jacastellanoa@gmail.com>
2019-10-11 23:14:42 +0200

    Add Material Module

--- lab-02/src/app/app.module.ts
+++ lab-02/src/app/app.module.ts
@@ -2,9 +2,7 @@ import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -18,9 +16,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatListModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
